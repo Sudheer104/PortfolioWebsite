441e8636c3c6d9f28cd6da3e8cae3eb905e48f65
Sudheer Singh <singhsudheer2005@gmail.com>
2024-10-29 09:56:44 +0530

    Add about ans services page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import About from "./components/About"
-import Contact from "./components/Contact"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Navbar from "./components/navbar"
-import Projects from "./components/Project"
 import Services from "./components/Services"
+
 
 function App() {
   
@@ -15,10 +14,7 @@ function App() {
     <Hero/>
     <About/>
     <Services/>
-    <Projects/>
-    <Contact/>
     <Footer/>
-
     </>
   )
 }
